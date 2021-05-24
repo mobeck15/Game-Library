5db42e3f5dabbd1d9cb47ced02ee8a2be46f5dd0
Isaac Guerrero <mobeck@gmail.com>
2021-05-24 12:21:20 -0700

    Refactor to remove 'game' text

--- launchapp.py
+++ launchapp.py
@@ -1,16 +1,15 @@
 import time, subprocess, os, datetime, sys
 
-#cd \games\shortcuts\code
 #Launch Command: python launchapp.dev.py 4011 Epic
 
-from modules.launchparts import get_game_data, write_log_data, print_game_data, launchgame, saverecord
+from modules.launchparts import get_app_data, write_log_data, print_app_data, launchapp, saverecord
 from modules.launchparts import printtime, printelapses, captureendtime, getrating, getstatus
 
 datafile=os.path.dirname(__file__) + r"\data.json"
 
-game=get_game_data(sys.argv[1],filepath=datafile)
+appdata=get_app_data(sys.argv[1],filepath=datafile)
 
-print(print_game_data(game))
+print(print_app_data(appdata))
 
 datatype='Add Time'
 
@@ -20,11 +19,11 @@ print(printtime(start,'Starting at '))
 logfilename= os.path.dirname(__file__) + r'\log' + time.strftime("%Y", time.localtime(start)) + '-' + time.strftime("%m", time.localtime(start)) + '.csv'
 print(logfilename)
 
-write_log_data(logfilename,"Start",start,game['name'],game['id'],game['platform'])
+write_log_data(logfilename,"Start",start,appdata['name'],appdata['id'],appdata['platform'])
 
-print("Running " + game['name'])
+print("Running " + appdata['name'])
 
-launchgame(game,sys.argv)
+launchapp(appdata,sys.argv)
 
 end=captureendtime(start,mintime=120,verbose=True)
 elapsed=end-start
@@ -35,8 +34,8 @@ notes=input("Any Notes, (include Keywords - Idle Beat Cheat) ")
 rating=getrating()
 status=getstatus()
 
-write_log_data(logfilename,"Stop ",end,game['name'],game['id'],game['platform'],notes,rating,status)
+write_log_data(logfilename,"Stop ",end,appdata['name'],appdata['id'],appdata['platform'],notes,rating,status)
 
-print(saverecord(game,elapsedmin,datatype,notes,status,rating))
+print(saverecord(appdata,elapsedmin,datatype,notes,status,rating))
 
 input("Press Enter to continue...")
