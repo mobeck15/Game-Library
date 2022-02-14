438023beca407c0d0858aa75f0856e5275b60943
Isaac Guerrero <mobeck@gmail.com>
2022-02-13 21:19:18 -0800

    Adding playnext2 page

---
+++ server/playnext2.php
@@ -0,0 +1,51 @@
+<?php
+$GLOBALS['rootpath']=$GLOBALS['rootpath'] ?? ".";
+require_once $GLOBALS['rootpath']."/inc/php.ini.inc.php";
+require_once $GLOBALS['rootpath']."/inc/functions.inc.php";
+require_once $GLOBALS['rootpath']."/inc/topx.class.php";
+
+$title="Play Next";
+echo Get_Header($title);
+
+$calculations=getCalculations();
+
+$calculations=reIndexArray($calculations,"Game_ID");
+
+$topxobj= new topx($calculations);
+
+//$filter = $topxobj->filterlist();
+
+$totalranks=array();
+
+foreach ($topxobj->statlist() as $stat) {
+	$list = $topxobj->gettopx($stat);
+	foreach ($list as $key => $item){
+		$totalranks[$item]["ranks"] = ($totalranks[$item]["ranks"] ?? 0) + count($list)-$key;
+		$sortranks[$item]=$totalranks[$item]["ranks"];
+		$totalranks[$item]["id"]=$item;
+	}
+	echo $topxobj->displaytop($list,$stat);
+}
+
+array_multisort($sortranks, SORT_DESC, $totalranks);		
+
+
+$output ="<table>";
+$output .="<thead><tr><th>Ranks</th><th>Title</th></tr></thead>";
+$output .="<tbody>";
+foreach($totalranks as $item){
+	$output .="<tr>";
+	$output .="<tr class='".$calculations[$item["id"]]['Status']."'>";
+	$output .="<td>".$item["ranks"]."</td>";
+	$output .="<td><a href='viewgame.php?id=".$item["id"]."'>".$calculations[$item["id"]]["Title"]."</a></td>";
+	$output .="</tr>";
+}
+$output .="</tbody>";
+$output .="</table>";
+
+echo $output;
+
+echo Get_Footer(); 
+
+
+?>
